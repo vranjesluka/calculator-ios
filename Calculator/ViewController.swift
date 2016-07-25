0e05f81f38eb77918b7f3892cab5e1dca06e4820
vranjesluka <vranjes.luka@gmail.com>
2016-07-25 21:00:44 +0200

    first lesson: writing digit on a display

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var isTyping: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if (isTyping) {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            isTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
